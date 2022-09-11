c7faf8eb4f935359e37d28f0172d7e56ae9d1254
Revolver <maxylakers@gmail.com>
2022-09-11 18:33:53 +0100

    chore: reorganise code in modules

---
+++ module/variables.tf
@@ -0,0 +1,9 @@
+variable "admin_username" {
+    description = "VM user name to log into VM resource(s) in azure"
+  
+}
+
+variable "admin_password" {
+    description = "VM Passwprd to log into VM resource(s) in azure"
+  
+}
